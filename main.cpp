6369e7874955850eefc533a6583895b5ac85944f
Chris Bruno <Chris234k@gmail.com>
2018-11-26 16:13:54 -0500

    I don't need to create a console at all, it'll just output to my console (probably because of the makefile -Wl being removed)

--- main.cpp
+++ main.cpp
@@ -250,60 +250,6 @@ int main(int argc, char* args[])
             }
         }
     }
-    
-#ifdef _WIN32
-    { // create debug console
-        // TODO(chris) this should be debug builds only
-        // Get the window handle (we need it below)
-        SDL_SysWMinfo systemInfo;
-        SDL_VERSION(&systemInfo.version);
-        SDL_GetWindowWMInfo(gWindow, &systemInfo);
-
-        HWND handle = systemInfo.info.win.window;
-        
-        // Source: https://stackoverflow.com/questions/8482363/c-sdl-debugging-with-console-window
-        int hConHandle;
-        intptr_t lStdHandle;
-        CONSOLE_SCREEN_BUFFER_INFO coninfo;
-        FILE *fp;
-
-        // allocate a console for this app
-        AllocConsole();
-
-        // set the screen buffer to be big enough to let us scroll text
-        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
-        coninfo.dwSize.Y = 500;
-        SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coninfo.dwSize);
-
-        // redirect unbuffered STDOUT to the console
-        lStdHandle = (intptr_t)GetStdHandle(STD_OUTPUT_HANDLE);
-        hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-        fp = _fdopen( hConHandle, "w" );
-        *stdout = *fp;
-        setvbuf( stdout, NULL, _IONBF, 0 );
-
-        // redirect unbuffered STDIN to the console
-        lStdHandle = (intptr_t)GetStdHandle(STD_INPUT_HANDLE);
-        hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-        fp = _fdopen( hConHandle, "r" );
-        *stdin = *fp;
-        setvbuf( stdin, NULL, _IONBF, 0 );
-
-        // redirect unbuffered STDERR to the console
-        lStdHandle = (intptr_t)GetStdHandle(STD_ERROR_HANDLE);
-        hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-        fp = _fdopen( hConHandle, "w" );
-        *stderr = *fp;
-        setvbuf( stderr, NULL, _IONBF, 0 );
-
-        // make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog point to console as well
-        std::ios::sync_with_stdio();
-
-        //Keep our window in focus
-        SetForegroundWindow(handle);                        // Slightly Higher Priority
-        SetFocus(handle);                                   // Sets Keyboard Focus To The Window
-    }
-#endif
 
     { // load media
         gFont = TTF_OpenFont("lazy.ttf", 28);
